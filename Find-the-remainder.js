5424ee4517dc909fa6ed9a5a846cd7a8fc0384af
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:43:33 -0400

    Update and rename Find-the-remainder to Find-the-remainder.js

--- Find-the-remainder.js
+++ Find-the-remainder.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
+/* https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
 
 Task:
 Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
@@ -21,7 +21,7 @@ result = 0 (remainder of `0 / -1`)
 n = 0
 
 Solution:
-
+*/
 function remainder(n, m){
   if (n>m){
      return n%m
