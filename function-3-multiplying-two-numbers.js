95f4d2365856fbac8454d00f539cb59ea49aa1f2
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:53:49 -0400

    Update and rename function-3-multiplying-two-numbers to function-3-multiplying-two-numbers.js

--- function-3-multiplying-two-numbers.js
+++ function-3-multiplying-two-numbers.js
@@ -1,11 +1,11 @@
-https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
+/* https://www.codewars.com/kata/523b66342d0c301ae400003b/train/javascript
 
 Instructions:
 
 Implement a function which multiplies two numbers.
 
 Solution: 
-
+*/
 function multiply(a,b){
   let product = (a * b)
   return product
