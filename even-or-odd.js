f90f6943472a03c291bc6fca449de061dbc5b44b
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:51:09 -0400

    Update and rename even-or-odd to even-or-odd.js

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+/* https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 
 Even or Odd
 
@@ -7,7 +7,7 @@ Output
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 Solution:
-
+*/
 function even_or_odd(number) {
   //condition for even
   //condition for odd
