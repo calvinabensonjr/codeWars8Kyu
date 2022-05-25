ef9c35cec1d6afcc2ca8c83b15d01042c66817aa
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:59:33 -0400

    Update and rename makeUpperCass to makeUpperCase.js

--- makeUpperCase.js
+++ makeUpperCase.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+/* https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
 
 MakeUpperCase
 
@@ -8,7 +8,7 @@ Instructions
 Write a function which converts the input string to uppercase.
 
 Solution:
-
+*/
 function makeUpperCase(str) {
   // Code here
   console.log(str.toUpperCase())
