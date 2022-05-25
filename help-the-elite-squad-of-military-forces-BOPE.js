ddd60e994a2468e448dcdc235790bfff7efa0eaf
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:56:23 -0400

    Update and rename help-the-elite-squad-of-military-forces-BOPE to help-the-elite-squad-of-military-forces-BOPE.js

--- help-the-elite-squad-of-military-forces-BOPE.js
+++ help-the-elite-squad-of-military-forces-BOPE.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/5ab52526379d20736b00000e/train/javascript
+/* https://www.codewars.com/kata/5ab52526379d20736b00000e/train/javascript
 
 Help the Elite Squad of Brazilian forces BOPE
 
@@ -23,7 +23,7 @@ Example:
 The return Will always be an integer so as the params.
 
 Solution:
-
+*/
 magNumber = info => Math.ceil(info[1] * 3/ {'PT92' : 17, 'M4A1' : 30, 'M16A2' : 30, 'PSG1' : 5}[info[0]])
 
 
