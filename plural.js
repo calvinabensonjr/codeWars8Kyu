79a04cd5a156558bcd6a7e65f72a48653facc083
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 17:02:06 -0400

    Update and rename plural to plural.js

--- plural.js
+++ plural.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+/* https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
 
 Plural
 
@@ -9,6 +9,9 @@ We need a simple function that determines if a plural is needed or not. It shoul
 You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
 
 All values will be positive integers or floats, or zero.
+
+Solution:
+*/
 
 function plural(n) {
   // ...
