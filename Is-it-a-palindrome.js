cd3bde2328ab824c8cd969c689a2da3ba5719430
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 16:44:09 -0400

    Update and rename Is-it-a-palindrome to Is-it-a-palindrome.js

--- Is-it-a-palindrome.js
+++ Is-it-a-palindrome.js
@@ -1,10 +1,11 @@
-https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+/* https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
 
 Instructions:
 Write a function that checks if a given string (case insensitive) is a palindrome.
 
 
 Solution:
+*/
 
 function isPalindrome(x) {
   //first step: put each character of y into and array. Ex. 'abc' becomes ['A' ,'b' ,'c']
