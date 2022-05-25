ebf84ed32429a70151d197623229741ed50ac9a7
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-05-25 17:11:31 -0400

    Update and rename you-can't-code-under-pressure-#1 to you-can't-code-under-pressure-#1.js

--- you-can't-code-under-pressure-#1.js
+++ you-can't-code-under-pressure-#1.js
@@ -1,4 +1,4 @@
-https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+/* https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
 
 
 Instructions
@@ -6,7 +6,7 @@ Instructions
 Code as fast as you can! You need to double the integer and return it.
 
 Solution:
-
+*/
 function doubleInteger(i) {
   // i will be an integer. Double it and return it.
  let double = i * 2
