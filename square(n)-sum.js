d323870a0e1678e0ff3491f4b8bca42480c1a4dc
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-06-07 16:38:20 -0400

    Create square(n)-sum.js

---
+++ square(n)-sum.js
@@ -0,0 +1,13 @@
+/* https://www.codewars.com/kata/54a91a4883a7de5d7800009c/train/javascript
+
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+solution:
+*/
+return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
