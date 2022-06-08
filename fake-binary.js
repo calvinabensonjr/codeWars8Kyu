fad70b71cd985ecbfe6a623ba5e2e7929ca05714
Calvin Allen Benson Jr <60082928+calvinabensonjr@users.noreply.github.com>
2022-06-08 15:55:19 -0400

    Create fake-binary.js

---
+++ fake-binary.js
@@ -0,0 +1,16 @@
+/* https://www.codewars.com/kata/57eae65a4321032ce000002d/train/javascript
+
+
+Instuctions:
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+Solution:
+*/
+
+function fakeBin(x){
+  
+  return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
